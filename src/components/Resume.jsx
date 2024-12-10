20bf0ead82d3c9e12e1250828741c1f4cec48d63
KieranMueller <kieran98mueller@gmail.com>
2024-12-09 21:59:44 -0600

    resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 export default function Resume() {
     return (
         <div id='resume-container-1'>
-            <div className='download-div'>Download My Resume (Old - Last updated Aug '23)</div>
+            <div className='download-div'>
+                Download My Resume (Last updated Sep '24)
+            </div>
             <img src='resume.png' alt='resume' id='resume-img' />
             <div className='resume-btn-div'>
                 <a
-                    href='resume-personal.pdf'
+                    href='(SEP 2024) resume-kieran-mueller.pdf'
                     download={'Kieran Mueller Resume'}>
                     <button className='btn btn-primary'>Download PDF</button>
                 </a>
